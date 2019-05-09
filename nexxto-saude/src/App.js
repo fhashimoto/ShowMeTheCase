702971c4c98d506b2ff5cab85299700722372aab
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-05-09 11:28:16 -0300

    criando component home

--- nexxto-saude/src/App.js
+++ nexxto-saude/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import NavBar from './View/NavBar';
+import Home from './View/Home';
 
 
-function App() {
-  return (
-    <div>
-      <NavBar/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <NavBar/>
+        <Home/>
+      </div>
+    );
+  }
 }
 
 export default App;
